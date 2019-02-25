61c8d12c157564aeffbaa0f97656c235d9f2833a
Lana <svetlana.vaikute@gmail.com>
2019-02-24 21:27:05 -0800

    Added Homework - Comment Count Check

--- qa2_2/src/test/java/ArticleTest.java
+++ qa2_2/src/test/java/ArticleTest.java
@@ -1,59 +1,100 @@
-import org.junit.jupiter.api.AfterAll;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class ArticleTest {
     private final String URL = "https://www.delfi.lv";
     private final By TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'd-inline')]");
-    private final By COMMENT = By.xpath(".//a[contains(@class, 'text-red-ribbon')]");
+    private final By COMMENT_ARTICLE_PAGE = By.xpath(".//a[contains(@class, 'text-red-ribbon')]");
     private final By COMMENT_PAGE_TITLE = By.xpath(".//h1[@class='article-title']/a");
+    private final By COMMENT_COUNT_HOME_PAGE = By.xpath(".//a[contains(@class, 'comment-count')]");
+    private final By COMMENT_COUNT_COMMENT_PAGE = By.xpath(".//span[@class='type-cnt']");
 
     private WebDriver driver;
+
+    @BeforeEach
+    public void openHomePage() {
+
+        System.setProperty("webdriver.chrome.driver", "D:/projects/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL);
+    }
+
     @Test
     public void articleTitleCheck() {
-        //0. set driver path
-        System.setProperty("webdriver.chrome.driver", "D:/projects/chromedriver.exe");
-        //1. Open browser
-        driver = new ChromeDriver();
-        //2. Max full screen
-        driver.manage().window().maximize();
-        //3. Open Home page
-        driver.get(URL);
-        //4. Find first article title
+
         WebElement homePageTile = driver.findElement(TITLE);
-        //5. Save Article title
         String txtHomePageTitle = homePageTile.getText();
-        //6. Click on article
+
         homePageTile.click();
-        //7. Find title
+
         WebElement articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE);
-        //8. save to string
         String txtArticlePageTitle = articlePageTitle.getText();
-        //9. assert article title
+
         Assertions.assertEquals(txtHomePageTitle, txtArticlePageTitle, "Article Title is wrong");
-        //10. Find comment count
-        WebElement lnkComment = driver.findElement(COMMENT);
-        //11. click on comment count
+
+        WebElement lnkComment = driver.findElement(COMMENT_ARTICLE_PAGE);
+
         lnkComment.click();
-        //12. Find title
+
         WebElement commentPageTile = driver.findElement(COMMENT_PAGE_TITLE);
-        //13. save to string
         String txtCommentPageTitle = commentPageTile.getText();
-        //14. assert article title
+
         Assertions.assertEquals(txtHomePageTitle, txtCommentPageTitle, "Comment Page Article Title is wrong");
 
+    }
+
+    @Test
+    public void articleCommentCountCheck() {
+
+        WebElement commentCount = driver.findElement(COMMENT_COUNT_HOME_PAGE);
+
+        String txtCommentCount = commentCount.getText();
+        txtCommentCount = txtCommentCount.replace("(", "");
+        txtCommentCount = txtCommentCount.replace(")", "");
+        System.out.println(txtCommentCount);
+
+        WebElement homePageTitle = driver.findElement(TITLE);
+        homePageTitle.click();
+
+        WebElement articleCommentCount = driver.findElement(COMMENT_ARTICLE_PAGE);
+
+        String txtArticleCommentCount = articleCommentCount.getText();
+        txtArticleCommentCount = txtArticleCommentCount.replace("(", "");
+        txtArticleCommentCount = txtArticleCommentCount.replace(")", "");
+        System.out.println(txtArticleCommentCount);
+
+        Assertions.assertEquals(txtCommentCount, txtArticleCommentCount, "Wrong Comment count!");
+
+        articleCommentCount.click();
+
+        List<WebElement> counts = driver.findElements(COMMENT_COUNT_COMMENT_PAGE);
+        int sum = 0;
+        for (WebElement li : counts) {
+            System.out.println(li.getText());
+            String s = li.getText().replace("(", "");
+            sum = sum + Integer.parseInt(s.replace(")", ""));
+        }
+
+        System.out.println(sum);
+        String txtComments = Integer.toString(sum);
+        Assertions.assertEquals(txtCommentCount, txtComments, "Wrong Comment count!");
 
     }
 
     @AfterEach
-    public void browserClose(){
-        //15. close browser
+    public void browserClose() {
+
         driver.close();
     }
 }
